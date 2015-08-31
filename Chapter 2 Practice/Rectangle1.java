36406768bf37ca35cb8f81e37666dad65faf5157
Connor Chong <crchong1@NN123-HA-13.HS.sd203.org>
2015-08-31 15:08:21 -0500

    k

---
+++ Chapter 2 Practice/Rectangle1.java
@@ -0,0 +1,11 @@
+import java.awt.Rectangle;
+
+public class Rectangle1
+{
+    public static void main(String[] args)
+    {
+        Rectangle box = new Rectangle (5,10,20,30);
+        box.add(5,5);
+        System.out.println(box);   
+    }
+}
